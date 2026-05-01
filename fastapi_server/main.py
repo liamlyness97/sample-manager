6a794f37f42d8343d0f070642eb5b837a1072630
Liam Lyness <liamlyness97@gmail.com>
2026-05-01 16:27:09 +0100

    Added return of info from upload

--- fastapi_server/main.py
+++ fastapi_server/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi_server.routes import files
 
+
 app = FastAPI()
 
 app.include_router(files.router)
--- fastapi_server/routes/files.py
+++ fastapi_server/routes/files.py
@@ -1,4 +1,5 @@
 import shutil
+import librosa
 from pathlib import Path
 from fastapi import APIRouter, UploadFile
 
@@ -11,4 +12,21 @@ router = APIRouter(prefix="/files", tags=["files"])
 async def upload(file: UploadFile):
     with open(UPLOAD_DIR / file.filename, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
-    return {"filename": file.filename, "status": 200}
+
+    y, sr = librosa.load(UPLOAD_DIR / file.filename)
+    tempo, beats = librosa.beat.beat_track(y=y, sr=sr)
+    duration = f"{librosa.get_duration(y=y, sr=sr):.2f} seconds"
+
+    return {
+        "filename": file.filename, 
+        "tempo": float(tempo[0]), 
+        "sample_rate": sr,
+        "duration": duration,  
+        "status": 200
+    }
+
+@router.get("/test")
+async def test_file():
+    y, sr = librosa.load(UPLOAD_DIR / 'bass_riff_rippy.wav')
+    tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
+    return {"temp": 'Estimated tempo: {:.2f}'.format(tempo[0])}
