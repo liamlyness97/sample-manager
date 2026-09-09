57200ce7857623386e64ae3dbb457bdb6bf550f3
Liam Lyness <liamlyness97@gmail.com>
2026-09-09 22:06:17 +0100

    Further work in refinement

--- fastapi_server/routes/files.py
+++ fastapi_server/routes/files.py
@@ -1,10 +1,10 @@
+import os
 import shutil
 import librosa
 from pathlib import Path
 from fastapi import APIRouter, UploadFile
 
-UPLOAD_DIR = Path("uploads")
-UPLOAD_DIR.mkdir(exist_ok=True)
+UPLOAD_DIR = Path(os.environ.get("UPLOAD_DIR", "../uploads"))
 
 router = APIRouter(prefix="/files", tags=["files"])
 
@@ -27,6 +27,6 @@ async def upload(file: UploadFile):
 
 @router.get("/test")
 async def test_file():
-    y, sr = librosa.load(UPLOAD_DIR / 'bass_riff_rippy.wav')
+    y, sr = librosa.load(UPLOAD_DIR / 'U8rLB63r5VVtCwTN0oZjSoklagKaJhC0/66f0f87d-a988-44bf-8b6a-836a5feefe64.wav')
     tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
     return {"temp": 'Estimated tempo: {:.2f}'.format(tempo[0])}
