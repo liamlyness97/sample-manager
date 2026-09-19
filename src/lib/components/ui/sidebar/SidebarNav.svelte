d31d0886315bba152b2780a1203cf4dd8ab4fd41
Liam Lyness <liamlyness97@gmail.com>
2026-09-19 10:52:09 +0100

    Sidebar component created with sidebar items

---
+++ src/lib/components/ui/sidebar/SidebarNav.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import SidebarNavItem from './SidebarNavItem.svelte';
+</script>
+
+<div class="flex h-full w-1/6 shrink-0 flex-col gap-2 bg-blue-300 px-4 py-24 text-sm">
+	<SidebarNavItem label="Dashboard" icon="hugeicons:home-07" link="/dashboard" />
+	<SidebarNavItem label="Samples" icon="hugeicons:audio-wave-02" link="/dashboard/samples" />
+	<SidebarNavItem label="Samples Types" icon="hugeicons:folder-01" link="/dashboard/sample-types" />
+	<SidebarNavItem label="Collections" icon="hugeicons:layers-01" link="/dashboard/collections" />
+</div>
---
+++ src/lib/components/ui/sidebar/SidebarNavItem.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { page } from '$app/state';
+
+	let { icon, label, link }: { icon: string; label: string; link: string } = $props();
+</script>
+
+<a
+	href={link}
+	class="flex items-center gap-2 rounded-lg px-2 py-2 text-white duration-200 ease-out hover:bg-blue-100 {page
+		.url.pathname === link
+		? 'bg-blue-100'
+		: ''}"
+>
+	<iconify-icon class="text-xl" {icon}></iconify-icon>
+	<span>{label}</span></a
+>
