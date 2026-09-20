6bb60fc81a776d3efbecb7d2a84fcf3aa5440c4a
Liam Lyness <liamlyness97@gmail.com>
2026-09-20 16:21:30 +0100

    Few style tweaks

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -11,22 +11,22 @@
 			<div>Upload</div>
 		</div>
 	</div>
-	<div class="grid grid-cols-6 gap-4 py-8">
-		<div class="col-span-4 flex flex-col gap-4 rounded-lg bg-blue-300 p-4">
+	<div class="grid grid-cols-12 gap-4 py-8">
+		<div class="col-span-8 flex flex-col gap-4 rounded-xl bg-blue-300 p-8">
 			<p class="text-sm tracking-wider text-white/50 uppercase">All Samples</p>
 			<p class="text-7xl text-white">{data.sampleCount}</p>
 			<p class="text-sm tracking-wider text-white/50 uppercase">29.3GB</p>
 		</div>
-		<div class="col-span-2 flex flex-col gap-4 rounded-lg bg-blue-100 p-4">
+		<div class="col-span-4 flex flex-col gap-4 rounded-xl bg-blue-100 p-8">
 			<p class="text-sm tracking-wider text-white/50 uppercase">Recently Played</p>
 		</div>
-		<div class="col-span-2 flex flex-col gap-4 rounded-lg bg-blue-100 p-4">
-			<p class="text-sm tracking-wider text-white/50 uppercase">Genre</p>
+		<div class="col-span-4 flex flex-col gap-4 rounded-xl bg-blue-100 p-8">
+			<p class="text-sm tracking-wider text-white/50 uppercase">Collections</p>
 		</div>
-		<div class="col-span-2 flex flex-col gap-4 rounded-lg bg-blue-100 p-4">
+		<div class="col-span-4 flex flex-col gap-4 rounded-xl bg-blue-100 p-8">
 			<p class="text-sm tracking-wider text-white/50 uppercase">BPM</p>
 		</div>
-		<div class="col-span-2 flex flex-col gap-4 rounded-lg bg-blue-100 p-4">
+		<div class="col-span-4 flex flex-col gap-4 rounded-xl bg-blue-100 p-8">
 			<p class="text-sm tracking-wider text-white/50 uppercase">Key</p>
 		</div>
 	</div>
--- src/routes/dashboard/collections/[collectionId]/+page.svelte
+++ src/routes/dashboard/collections/[collectionId]/+page.svelte
@@ -13,7 +13,7 @@
 
 <div class="flex h-full w-full grow flex-col gap-4 overflow-hidden">
 	<div class="flex w-full flex-col gap-2">
-		<h2 class="text-4xl font-light text-white">Browse {collection.name} Collection</h2>
+		<h2 class="text-4xl font-light text-white">Browse {collection.name}</h2>
 		<div class="mt-2 flex justify-between text-white/50">
 			<p>{totalSamples} samples | sorted by recently added</p>
 			<div class="flex gap-2"></div>
@@ -29,10 +29,6 @@
 					bind:value={searchTerm}
 					placeholder="Search samples..."
 				/>
-			</div>
-			<div class="flex gap-2">
-				<button class="rounded-xl border border-white/20 px-4 py-2 text-white/50"> All </button>
-				<button class="rounded-xl border border-white/20 px-4 py-2 text-white/50"> Kicks </button>
 			</div>
 		</div>
 
