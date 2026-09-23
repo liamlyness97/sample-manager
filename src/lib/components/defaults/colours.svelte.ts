59f73be3a2d8a8e598a3497d4ba0c6896352e028
Liam Lyness <liamlyness97@gmail.com>
2026-09-23 11:54:33 +0100

    Added default set of highlight colours

---
+++ src/lib/components/defaults/colours.svelte.ts
@@ -0,0 +1,25 @@
+type Colour = {
+    name: string;
+    colour: string
+}
+
+export const defaultColours: Colour[] = [
+    { name: 'Graphite', colour: '#374151' },
+    { name: 'Slate', colour: '#64748B' },
+    { name: 'Rose', colour: '#F43F5E' },
+    { name: 'Coral', colour: '#FF6B6B' },
+    { name: 'Tangerine', colour: '#F97316' },
+    { name: 'Amber', colour: '#F59E0B' },
+    { name: 'Sunflower', colour: '#FACC15' },
+    { name: 'Lime', colour: '#84CC16' },
+    { name: 'Emerald', colour: '#10B981' },
+    { name: 'Teal', colour: '#14B8A6' },
+    { name: 'Cyan', colour: '#06B6D4' },
+    { name: 'Sky', colour: '#0EA5E9' },
+    { name: 'Blue', colour: '#3B82F6' },
+    { name: 'Indigo', colour: '#6366F1' },
+    { name: 'Violet', colour: '#8B5CF6' },
+    { name: 'Purple', colour: '#A855F7' },
+    { name: 'Fuchsia', colour: '#D946EF' },
+    { name: 'Pink', colour: '#EC4899' },
+]
