f288782056f8df999a6471c9220bcd55ce96c27e
Liam Lyness <liamlyness97@gmail.com>
2026-09-24 15:20:31 +0100

    user and session added to locals type

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,17 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
+import type { auth } from '$lib/auth/auth';
+
+type AuthSession = typeof auth.$Infer.Session;
+
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			// Only set by hooks.server.ts when the request has a valid session
+			user?: AuthSession['user'];
+			session?: AuthSession['session'];
+		}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
